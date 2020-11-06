a5a9e0906d9167ab89753675b084d5b5715a551e
cspackman <connorspackman21@gmail.com>
2020-11-06 10:53:19 -0800

    finished snake

--- Lab 1030 Snake/snake.js
+++ Lab 1030 Snake/snake.js
@@ -21,31 +21,39 @@ Snake.prototype.move = function() {
   for (let i=0; i < this.segments.length; i++) {
     if (i===0) {
       this.segments[i] = new JSVector(this.loc.x, this.loc.y);
-    } else {
-      let dist = JSVector.subGetNew(this.segments[i], this.segments[i-1]);
-      dist.setMagintude(1);
-      this.segemnts[i] = JSVector.addGetNew(this.segments[i-1], dist);
     }
+    if(i!==0){
+    var dist = JSVector.subGetNew(this.segments[i],this.segments[i-1]);
+    dist.setMagnitude(20);
+    this.segments[i]=JSVector.addGetNew(dist,this.segments[i-1]);
   }
+}
 }
 
 Snake.prototype.render = function() {
   let ctx = game.ctx;
   for (let i = 0; i < this.segments.length; i++) {
-    ctx.strokeStyle = "rgba(255,0,0)" //this.clr;
+    ctx.lineWidth= 20-i;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "rgba(255,0,0,.5)" //this.clr;
     ctx.fillStyle = this.clr;
     ctx.beginPath();
-    ctx.arc(this.segments[i].x, this.segments[i].y, 10, Math.PI * 2, 0, false);
+    //ctx.arc(this.segments[i].x, this.segments[i].y, 10, Math.PI * 2, 0, false);
+    ctx.moveTo(this.segments[i].x,this.segments[i].y);
+    if(i!==0){
+    ctx.lineTo(this.segments[i-1].x,this.segments[i-1].y)
+  }
     ctx.stroke();
     ctx.fill();
   }
   ctx.strokeStyle = "rgba(255,0,0)" //this.clr;
   ctx.fillStyle = this.clr;
   ctx.beginPath();
-  ctx.arc(this.loc.x, this.loc.y, 10, Math.PI * 2, 0, false);
+  //ctx.arc(this.loc.x, this.loc.y, 10, Math.PI * 2, 0, false);
   ctx.stroke();
   ctx.fill();
 }
+
 Snake.prototype.checkEdges = function() {
   let canvas = game.canvas;
   if (this.loc.x > canvas.width) this.vel.x = -this.vel.x; // wrap around from right to left
